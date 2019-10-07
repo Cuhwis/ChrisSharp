6e0f367975b058995463ddeb9679f863b6fb46ba
Christopher Ruaboro <Christopher.Ruaboro@outlook.com>
2019-10-06 17:59:52 -0700

    Add project files.

---
+++ ChrisSharpBot/Modules/Commands.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChrisSharpBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+        [Command("CoolerChris")]
+        public async Task Chris()
+        {
+            await ReplyAsync("Chris R. of course");
+        }
+    }
+}
