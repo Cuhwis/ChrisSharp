32d1be0d3857d9c15e7514757ee98b52f520e5e8
wwstudent <wwstudent@MS-9D12CH2>
2019-11-04 16:20:43 -0800

    Fixed layout links to go to actual link

--- ChrisSharp/Controllers/HomeController.cs
+++ ChrisSharp/Controllers/HomeController.cs
@@ -19,6 +19,14 @@ namespace ChrisSharp.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Commands()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
